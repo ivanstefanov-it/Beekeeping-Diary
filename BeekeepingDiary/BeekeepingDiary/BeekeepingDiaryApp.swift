572baf4a7713d06ba00badf4599626cb4f48d231
Ivan <ivanstefanov.it@gmail.com>
2025-12-05 10:01:30 +0200

    Initial comit

---
+++ BeekeepingDiary/BeekeepingDiary/BeekeepingDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeekeepingDiaryApp.swift
+//  BeekeepingDiary
+//
+//  Created by Ivan Stefanov on 5.12.25.
+//
+
+import SwiftUI
+
+@main
+struct BeekeepingDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
