13d6d3cd89319cce3a416e6bd928b93fec023461
Ivan <ivanstefanov.it@gmail.com>
2026-02-11 19:30:20 +0200

    added apiary details View and ViewModel

---
+++ BeekeepingDiary/BeekeepingDiary/ViewModels/ApiaryDetailViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ApiaryDetailViewModel.swift
+//  BeekeepingDiary
+//
+//  Created by Ivan Stefanov on 11.02.26.
+//
+
+import Foundation
---
+++ BeekeepingDiary/BeekeepingDiary/Views/ApiaryDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ApiaryDetailView.swift
+//  BeekeepingDiary
+//
+//  Created by Ivan Stefanov on 11.02.26.
+//
+
+import SwiftUI
+
+struct ApiaryDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ApiaryDetailView()
+}
